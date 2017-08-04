5f4f3fe25186987a94d77152ef8f561a59e26c7e
maxlauhi <maxlauhi@gmail.com>
2017-08-04 23:37:40 +0800

    mapping add

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 
 class Model(dict):
-	pass
+	__metaclass__ = ModelMetaclass
+
+	def __init__(self, **kw):
+	    super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
 
 class ModelMetaclass(type):
-	pass
+	def __new__(cls, name, bases, attrs):
+		mapping = ...
+		primary_key = ...
+		__table__ = cls.__table__
+
+		attrs['__mapping__'] = mapping
+		attrs['__primary_key__'] = __primary_key__
+		attrs['__table__'] = __table__
+		return type.__new__(cls, name, bases, attrs)
